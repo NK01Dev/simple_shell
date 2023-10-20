925c8050b8d94dddc27ec71cab05c8a4011aef4a
ACHERKI-YOUSSEF <youssef.acherki@um5r.ac.ma>
2023-10-20 15:12:33 -0700

    c

--- main.c
+++ main.c
@@ -25,17 +25,14 @@ return (stts);
 indx++;
 cmnd = spreader(line);
 if (!cmnd)
-{
-free(line);
 continue;
-}
-
 
 if (is_builtinside(cmnd[0]))
 {
 if (_strcmp(cmnd[0], "setenv") == 0 || _strcmp(cmnd[0], "unsetenv") == 0)
 {
 handle_builtinside(cmnd, argv, &stts,indx);
+freeTable(cmnd);
 }
 else
 {
@@ -46,8 +43,6 @@ else
 {
 stts = _exec(cmnd, argv, indx);
 }
-free(line);
-freeTable(cmnd);
 }
 return 0;
 }
